a3f797aafd0615dbe638978ed116c4e8c58a5854
angelahack1 <angelakimichellle@hotmail.com>
2023-11-18 17:56:49 -0600

    Tercer commit, por angelahack1

--- index.mjs
+++ index.mjs
@@ -8,10 +8,6 @@ const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.static(path.join(__dirname, './')));
 
-app.post('/contact_form', (req, res) => {
-    console.log('Req received: ', req);
-});
-
 app.listen(80, function() {
     console.log('Server is running on port 80');
 });

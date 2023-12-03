0e4fd78afc6114be161252e105e72994261520a8
angelahack1 <angelakimichellle@hotmail.com>
2023-12-02 18:03:56 -0600

    fixing utils for comments, angelahack1

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,7 @@
 /* Java Script Utilities for Artecnología */
 /* Author: Ángela López Mendoza           */
-/* Copyright Artecnología 2023            */
-/* AixKare/Artecnología 11/2023           */
+/* Copyright Aixkare 2023            */
+/* AixKare 12/2023           */
 
 var _headerId = null;
 
@@ -27,7 +27,66 @@ function handleClickOutside(event) {
     }
 }
 
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function validateForm(form) {
+    var isValid = true;
+    var elements = form.elements;
+
+    for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+
+        if (element.type === "textarea" && element.value.trim() === "") {
+            alert("Campo de texto vacio: Porfavor introduzca el contenido de su consulta/duda/comentario...");
+            isValid = false;
+            break;
+        }
+
+        if (element.type === "email" && element.value.length === 0 ) {
+            alert("Campo de correo vacio: Porfavor introduzca su correo electrónico en el campo de correo...");
+            isValid = false;
+            break;
+        } else if(element.type === "email") {
+            if(!validateEmail(element.value)) {
+                alert("Campo de correo incorrecto: Porfavor introduzca correctamente su correo electrónico...");
+                isValid = false;
+            }
+        }
+    }
+    return isValid;
+}
+
+function handleFormSubmit(event) {
+   var form = event.target;
+   var data = {
+     email: form.formControlMail.value,
+     comment: form.formControlTextarea.value
+   };
+    window.top.location.href = form.getAttribute('action')+'?email='+form.formControlMail.value+'&comment='+form.formControlTextarea.value;
+ }
+
+function addValidationInClient() { 
+    var form = document.getElementById("contactForm");
+
+    if(typeof form != 'undefined' && form != null) {
+        console.log("Validation of form is gonna be loaded...");
+        form.addEventListener("submit", function(event) {
+            if (!validateForm(form)) {
+                event.preventDefault();
+            } else {
+                handleFormSubmit(event);
+                event.preventDefault();
+            }
+        });
+        console.log("Validation of form loaded OK.");
+    }
+}
+
 $(function() {
     _headerId = $("#headerId");
     $(document).on('click', handleClickOutside);
+    addValidationInClient();
 });
